8e0b7b552919191621d983c42c226e1f4593bf1d
oscar <oscarada87@gmail.com>
2025-07-14 15:57:08 +0800

    15-feat: delete section

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -9,19 +9,21 @@ class SectionsController < BaseController
         return_error(status: 422, code: 42_200, error: StandardError.new(form.errors.full_messages.join(", ")), message: form.errors.full_messages.join(", "))
       end
     rescue ActiveRecord::RecordNotFound => e
-      return_error(status: 400, code: 40_001, error: e, message: "Course not found")
+      return_error(status: 400, code: 40_001, error: e, message: e.message)
     end
   end
 
   # DELETE /sections/:id
   def destroy
-    section = Section.find_by(id: params.require(:id))
-    if section.nil?
-      return_error(status: 400, code: 41_001, error: StandardError.new("Section not found"), message: "Section not found")
-      return
+    begin
+      service = DeleteSectionService.new(params.require(:id))
+      service.call
+      return_success(status: 200, code: 21_200, data: nil)
+    rescue ActiveRecord::RecordNotFound => e
+      return_error(status: 400, code: 40_001, error: e, message: e.message)
+    rescue DeleteSectionService::LastSectionError => e
+      return_error(status: 400, code: 40_002, error: e, message: e.message)
     end
-    section.destroy!
-    return_success(status: 200, code: 21_200, data: nil)
   end
 
   private
---
+++ app/services/delete_section_service.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+class DeleteSectionService
+  class LastSectionError < StandardError; end
+
+  def initialize(section_id)
+    @section = Section.find_by(id: section_id)
+  end
+
+  def call
+    raise ActiveRecord::RecordNotFound, "Section not found" if @section.nil?
+    course = @section.course
+    Section.transaction do
+      course.reload
+      if course.sections.count == 1
+        raise LastSectionError, "Cannot delete the last section of the course"
+      end
+      @section.destroy!
+    end
+    true
+  end
+end
